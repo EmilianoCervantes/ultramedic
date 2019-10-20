358515d8365000306bf757a8e7bf0ea212681d5d
EmilianoCervantes <em.cerv96@gmail.com>
2019-10-20 17:10:31 -0500

    Maquetado al 90%

---
+++ README.md
@@ -0,0 +1,7 @@
+# Página para Ultramedic
+Para Frank
+## Página
+## Mailchimp
+## Registros de formulario a Mailchimp
+## Diseño original (v.1.) por Memo
+### Diseño reemplazado, esta es la versión 2.0
---
+++ js/app.js
@@ -0,0 +1,24 @@
+$("#homeLink").click(function() {
+    $(".navLinkHomeP").removeClass('active');
+    $("#homeLink").addClass("active");
+});
+
+$("#misionVisionValoresLink").click(function() {
+    $(".navLinkHomeP").removeClass('active');
+    $("#misionVisionValoresLink").addClass("active");
+});
+
+$("#unidadesNegocioLink").click(function() {
+    $(".navLinkHomeP").removeClass('active');
+    $("#unidadesNegocioLink").addClass("active");
+});
+
+$("#dondeEstamosLink").click(function() {
+    $(".navLinkHomeP").removeClass('active');
+    $("#dondeEstamosLink").addClass("active");
+});
+
+$("#contactoLink").click(function() {
+    $(".navLinkHomeP").removeClass('active');
+    $("#contactoLink").addClass("active");
+});
